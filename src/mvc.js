e1d130c7cfbea5a47b3790576933e173a7606b25
tanakayuki <a0673040@gmail.com>
2013-01-26 12:12:32 +0900

    add util.evets

--- src/mvc.js
+++ src/mvc.js
@@ -1,5 +1,6 @@
 (function($, __global__) {
-    var mvc = {};
+    var mvc = {},
+        shift = Array.prototype.shift;
 
 
     /**
@@ -21,6 +22,83 @@
                 }
                 return keys;
             }
+        },
+
+        /**
+         * PubSub
+         */
+        events: {
+            handlers: {},
+
+            /**
+             * Register event
+             *
+             * @param {String}   evname  Event name
+             * @param {Function} func    Function
+             * @param {Boolean}  once    True if you want to fire this event only one time
+             */
+            on: function(evname, func, once) {
+                if (! this.handlers[evname]) {
+                    this.handlers[evname] = [];
+                }
+
+                this.handlers[evname].push({func: func, once: once});
+            },
+
+            /**
+             * Register once event
+             *
+             * @param {String}   evname  Event name
+             * @param {Function} func    Function
+             *
+             * @see mvc.util.events.on
+             */
+            once: function(evname, func) {
+                this.on(evname, func, true);
+            },
+
+            /**
+             * Fire registered events
+             *
+             * <pre>
+             * arg0:     event name
+             * arg1-n:   arguments to this event
+             * </pre>
+             *
+             */
+            fire: function() {
+                var evname = shift.apply(arguments);
+                var events = this.handlers[evname];
+                if (! events) {
+                    return;
+                }
+
+                for (var i = 0, len = events.length; i < len; i++) {
+                    if (! events[i]) {
+                        continue;
+                    }
+
+                    events[i].func.apply(this, arguments);
+                    if (events[i].once) {
+                        events[i] = null;
+                    }
+                }
+            },
+
+            /**
+             * Delete register event
+             *
+             * @param {String} evname  Event name. If you not set, delete all events.
+             */
+            off: function(evname) {
+                if (! evname) {
+                    this.handlers = {};
+                    return;
+                }
+
+                this.handlers[evname] = null;
+            }
+
         }
     };
 
--- test/mvc.test.js
+++ test/mvc.test.js
@@ -3,6 +3,94 @@
  */
 
 (function($, __global__) {
+    module('util.events', {
+        teardown: function() {
+            mvc.util.events.off();
+        }
+    });
+
+    test('make fire not register events', function() {
+        var events = mvc.util.events;
+        events.fire('nothing.event');
+        ok(true, "fire not register events");
+    });
+
+    test('make fire register events', function() {
+        expect(2);
+        var events = mvc.util.events;
+
+        events.on('test', function() {
+            ok(true, "register by on");
+        });
+
+        events.fire('test');
+        events.fire('test');
+    });
+
+    test('make fire register once events', function() {
+        expect(1);
+        var events = mvc.util.events;
+
+        events.once('test', function() {
+            ok(true, "register by on");
+        });
+
+        events.fire('test');
+        events.fire('test');
+    });
+
+    test('register some events in the same event name', function() {
+        expect(2);
+        var events = mvc.util.events;
+
+        events.on('test', function() {
+            ok(true, "register by on (1)");
+        });
+
+        events.on('test', function() {
+            ok(true, "register by on (2)");
+        });
+
+        events.fire('test');
+    });
+
+    test('events arguments', function() {
+        expect(2);
+        var events = mvc.util.events;
+
+        events.on('test', function(txt, num) {
+            equal(txt, "aaaa", "event argument0");
+            equal(num, 100,    "event argument1");
+        });
+
+        events.fire('test', "aaaa", 100);
+    });
+
+    test('events off', function() {
+        expect(1);
+        var events = mvc.util.events;
+
+        events.on('test', function() {
+            ok(! true, "should not call off events");
+        });
+
+        events.off('test');
+        events.fire('test');
+
+        events.on('test1', function() {
+            ok(! true, "should not call off event (1)");
+        });
+        events.on('test2', function() {
+            ok(! true, "should not call off event (2)");
+        });
+
+        events.off();
+        events.fire('test1');
+        events.fire('test2');
+
+        ok(true);
+    });
+
 
     module('Class');
 
