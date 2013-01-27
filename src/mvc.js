7fbd5fb171fa98875c2a13a4e1d7ae48052f9204
tanakayuki <a0673040@gmail.com>
2013-01-27 14:11:02 +0900

    Add mvc Model

--- src/mvc.js
+++ src/mvc.js
@@ -1,12 +1,17 @@
 (function($, __global__) {
     var mvc = {},
+        util,
+        Model,
+        View,
+        Controller,
+        Class,
         shift = Array.prototype.shift;
 
 
     /**
      * Utility for MVC
      */
-    mvc.util = {
+    util = mvc.util = {
         object: {
 
             create: function(obj) {
@@ -99,12 +104,19 @@
                 this.handlers[evname] = null;
             }
 
+        },
+
+        guid: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            }).toUpperCase();
         }
     };
 
 
     /**
-     * Class function.
+     * Class.
      *
      * <code>
      *   var Model = new Class();
@@ -117,7 +129,7 @@
      *   });
      * </code>
      */
-    mvc.Class = function () {
+    Class = mvc.Class = function () {
         var klass = function() {
             this.init.apply(this, arguments);
         };
@@ -134,7 +146,7 @@
             for (var i in obj) {
                 klass[i] = obj[i];
             }
-            if (extended) ectended(klass);
+            if (extended) extended(klass);
         };
 
         // Add instance properties
@@ -149,6 +161,146 @@
         return klass;
     };
 
+
+    /**
+     * Model class
+     *
+     * <code>
+     * var Person = Model.create()
+     * var person = Person.init();
+     * </code>
+     */
+    Model = mvc.Model = {
+        inherited: function() {},
+        created: function() {},
+        prototype: {
+            init: function() {}
+        },
+
+        create: function() {
+            var object = util.object.create(this);
+
+            object.parent = this;
+            object.fn = object.prototype;
+
+            object.created();
+            this.inherited(object);
+
+            return object;
+        },
+
+        init: function() {
+            var instance = util.object.create(this.prototype);
+            instance.parent = this;
+            instance.init.apply(instance, arguments);
+            return instance;
+        },
+
+        extend: function(o) {
+            var extended = o.extended;
+            $.extend(this, o);
+            if (extended) extended(this);
+        },
+
+        include: function(o) {
+            var included = o.included;
+            $.extend(this.prototype, o);
+            if (included) included(this);
+        }
+    };
+
+    // Add Model class properties
+    Model.extend({
+        records: {},
+
+        find: function(id) {
+            var record = this.records[id];
+            if (! record) {
+                throw('Not found record [id=' + id + ']');
+            }
+            return record.dup();
+        },
+
+        created: function() {
+            this.records = {};
+            this.attributes = [];
+        },
+
+        populate: function(values) {
+            this.records = {};
+
+            for(var i = 0, len = values.length; i < len; i++) {
+                var record = this.init(values[i]);
+                record.save();
+            }
+        }
+    });
+
+    // Add Model instance properties
+    Model.include({
+        newRecord: true,
+
+        init: function(attrs) {
+            if (attrs) this.load(attrs);
+        },
+
+        load: function(attrs) {
+            for(var name in attrs) {
+                this[name] = attrs[name];
+            }
+        },
+
+        destroy: function() {
+            delete this.parent.records[this.id];
+        },
+
+        /**
+         * Save record.
+         */
+        save: (function() {
+
+            var create = function() {
+                if (! this.id) this.id = util.guid();
+                this.newRecord = false;
+                this.parent.records[this.id] = this.dup();
+            };
+
+            var update = function() {
+                this.parent.records[this.id] = this.dup();
+            };
+
+            return function() {
+                this.newRecord ? create.call(this) : update.call(this);
+            };
+        }) (),
+
+        dup: function() {
+            return $.extend(true, {}, this);
+        },
+
+        attributes: function() {
+            var result = {};
+            for (var i in this.parent.attributes) {
+                var attr = this.parent.attributes[i];
+                result[attr] = this[attr];
+            }
+            result.id = this.id;
+            return result;
+        },
+
+        createRemote: function(url, callback) {
+            $.post(url, this.attributes(), callback);
+        },
+
+        updateRemote: function(url, callback) {
+            $.ajax({
+                url: url,
+                data: this.attributes(),
+                success: callback,
+                type: "PUT"
+            });
+        }
+    });
     // exports mvc to global
     __global__.mvc = mvc;
 
--- test/mvc.test.js
+++ test/mvc.test.js
@@ -131,6 +131,172 @@
         equal(person.say(), greeting, "Class enable to include function");
     });
 
+    module('Model');
+
+    test("Create model", function() {
+        var Person = mvc.Model.create();
+        ok(Person.records, "model has records");
+
+        var person = Person.init({
+            name: 'tanakayuki',
+            age:  25,
+            male: true
+        });
+
+        person.save();
+
+        var p = Person.find(person.id);
+        equal(p.name, 'tanakayuki');
+        equal(p.age,  25);
+        ok(p.male)
+    });
+
+    test("Throw exception when not found record", function() {
+        var Person = mvc.Model.create();
+        Person.attributes = ['name', 'age', 'male'];
+        throws(function() {
+            Person.find(1);
+        });
+    });
+
+    test("Generate records id", function() {
+        var Person = mvc.Model.create();
+
+        var person = Person.init({
+            name: 'tanaka',
+            age:  25,
+            male: true
+        });
+
+        ok(! person.id);
+        person.save();
+        ok(person.id);
+    });
+
+    test("Saving record should not effect to other record", function() {
+        var Person = mvc.Model.create();
+
+        var person1 = Person.init({
+            name: 'tanaka',
+            age:  25,
+            male: true
+        });
+        person1.save();
+
+        var person2 = Person.init({
+            name: 'akiyama',
+            age:  26,
+            male: false
+        });
+        person2.save();
+
+        equal(Person.find(person1.id).name, 'tanaka');
+        equal(Person.find(person1.id).age,  25);
+        ok(Person.find(person1.id).male);
+
+        equal(Person.find(person2.id).name, 'akiyama');
+        equal(Person.find(person2.id).age,  26);
+        ok(! Person.find(person2.id).male);
+
+        person1.name = 'tanaka - update';
+        person1.save();
+
+        equal(Person.find(person1.id).name, 'tanaka - update');
+        equal(Person.find(person2.id).name, 'akiyama');
+    });
+
+    test('populate', function() {
+        expect(7);
+        var Person = mvc.Model.create();
+
+        var personMaster = [
+            {
+                name: 'itanotomomi',
+                age:  22,
+                male: false
+            },
+            {
+                name: 'takahashiminami',
+                age:  23,
+                male: false
+            },
+            {
+                name: 'akimotoyashushi',
+                age: 48,
+                male: true
+            }
+        ];
+
+        // set attributes
+        Person.populate(personMaster);
+        var records = Person.records;
+
+        var cnt = 0;
+        for (var key in records) {
+            cnt++;
+            var record = records[key];
+            if (record.name === 'itanotomomi') {
+                equal(record.age, 22);
+                ok(! record.male);
+            } else if (record.name === 'takahashiminami') {
+                equal(record.age, 23);
+                ok(! record.male);
+            } else if (record.name === 'akimotoyashushi') {
+                equal(record.age, 48);
+                ok(record.male);
+            } else {
+                ok(false, 'Unknown name [name=' + record.name + ']');
+            }
+        }
+
+        equal(cnt, 3);
+    });
+
+    test('destroy', function() {
+        var Person = mvc.Model.create();
+
+        var person1 = Person.init({
+            name: 'tanaka',
+            age:  25,
+            male: true
+        });
+        person1.save();
+
+        var person2 = Person.init({
+            name: 'akiyama',
+            age:  26,
+            male: false
+        });
+        person2.save();
+
+        person1.destroy();
+
+        ok(Person.find(person2.id));
+        equal(Person.find(person2.id).name, 'akiyama');
+        equal(Person.find(person2.id).age, 26);
+        ok(! Person.find(person2.id).male);
+        throws(function() {
+            Person.find(person1.id);
+        });
+
+    });
+
+
+    test('attributes', function() {
+        var Person = mvc.Model.create();
+        Person.attributes = ['name', 'age', 'male'];
+
+        var person = Person.init({
+            name: 'tanakayuki',
+            male: true,
+            profession: 'IT'
+        })
+
+        equal(person.attributes().name,       'tanakayuki');
+        equal(person.attributes().age,        undefined);
+        equal(person.attributes().male,       true);
+        equal(person.attributes().profession, undefined);
+    });
 }) (this.jQuery, this);
 
 
